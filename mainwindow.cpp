59ce3b693d6b0c9bd7d322130d33dda25205e787
Fabian Pietsch <fabian@canvon.de>
2017-11-14 22:55:28 +0100

    [cvnirc-qt] MainWindow: Add a File->Quit menu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,11 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_action_Quit_triggered()
+{
+    // TODO: Close the UI more gently, provide ability to cancel quit.
+
+    // For now, this will have to suffice.
+    exit(0);
+}
